a786d8bf1830c1bd66e0144b4882844fe822675b
Adrian Staszewski <adrian.staszewski@icloud.com>
2018-12-14 23:11:08 +0100

    Fix

---
+++ ITManagement.Api/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITManagement.Core.Model;
+using ITManagement.Infrastructure.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _service;
+
+        public UsersController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{email}")]
+        public async Task<IActionResult> Get(string email)
+        {
+            var user = await _service.GetAsync(email);
+            return Ok(user);
+        }
+
+        // GET api/values/5
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]User request)
+        {
+            await _service.AddAsync(request);
+            return StatusCode(204);
+        }
+    }
+}
